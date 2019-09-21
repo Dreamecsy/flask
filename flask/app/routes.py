1f9bb06a0d2317227cab1e27a8cc06e440d506c1
sychen <1162644871@qq.com>
2019-09-21 23:15:15 +0800

    创建了模板

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,20 +1,10 @@
-#从app模块中即从__init__.py中导入创建的app应用
+#导入模板模块
+from flask import render_template
 from app import app
 
-#建立路由，通过路由可以执行其覆盖的方法，可以多个路由指向同一个方法。
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'duke'}
-    html = '''
-    <html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-        </html>
-
-    '''
-    return html
+    #将需要展示的数据传递给模板进行显示
+    return render_template('index.html',title='我的',user=user)
